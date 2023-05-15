cf4c56c00d6a95997b81a78db4fe5bd3cafbe688
shtosti <hanna.hubarava@gmail.com>
2023-05-16 01:44:44 +0200

    complete training with 'post'

--- perplexities/create_table.py
+++ perplexities/create_table.py
@@ -1,40 +1,6 @@
-# import re
-# import pandas as pd
-
-
-# def extract_perplexities(log_file):
-#     validation_perplexities = []
-
-#     with open(log_file, 'r') as file:
-#         log_lines = file.readlines()
-
-#     for line in log_lines:
-#         match = re.search(r'ppl:\s+(\d+\.\d+)', line)
-#         if match:
-#             perplexity = float(match.group(1))
-#             validation_perplexities.append(perplexity)
-
-#     return validation_perplexities
-
-
-# log_files = ["baseline.log", "pre.log"]
-# all_perplexities = []
-
-# for log_file in log_files:
-#     perplexities = extract_perplexities(log_file)
-#     all_perplexities[log_file] = perplexities
-
-
-# df = pd.DataFrame(all_perplexities)
-# # steps = [500 * (i + 1) for i in range(len(df))]
-# # df.index = steps
-
-
-# df.to_csv('perplexity_results.csv', index=False)
-
-
 import re
 import pandas as pd
+import matplotlib.pyplot as plt
 
 
 def extract_perplexities(log_file):
@@ -52,7 +18,7 @@ def extract_perplexities(log_file):
     return validation_perplexities
 
 
-log_files = ["baseline.log", "pre.log"]
+log_files = ["baseline.log", "pre.log", "post.log"]
 all_perplexities = {}
 
 for log_file in log_files:
@@ -66,3 +32,16 @@ df.insert(0, "Step", steps)
 
 
 df.to_csv('perplexity_results.csv', index=False)
+
+# create a line chart based on the df
+plt.plot(df['Step'], df['baseline.log'], label='baseline', color='#FF00FF')
+plt.plot(df['Step'], df['pre.log'], label='pre', color = "#009999") 
+# plt.plot(df['Step'], df['post.log'], label='post', color = "#FFFF00")
+plt.xlabel('Step')
+plt.ylabel('Ppl')
+plt.legend()
+plt.savefig('perplexity_results.png')
+plt.show()
+
+
+
