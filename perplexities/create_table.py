6679d3614cf4bade43c868ea713b390ff109ab5d
shtosti <hanna.hubarava@gmail.com>
2023-05-16 11:19:47 +0200

    create table and visualize

--- perplexities/create_table.py
+++ perplexities/create_table.py
@@ -1,6 +1,7 @@
 import re
 import pandas as pd
 import matplotlib.pyplot as plt
+from itertools import zip_longest
 
 
 def extract_perplexities(log_file):
@@ -18,12 +19,19 @@ def extract_perplexities(log_file):
     return validation_perplexities
 
 
-log_files = ["baseline.log", "pre.log", "post.log"]
+log_files = ["baseline.log", "pre.log", "post.log", "pre2.log"]
 all_perplexities = {}
 
+
+max_length = 0
 for log_file in log_files:
     perplexities = extract_perplexities(log_file)
     all_perplexities[log_file] = perplexities
+    max_length = max(max_length, len(perplexities))
+
+for log_file, perplexities in all_perplexities.items():
+    if len(perplexities) < max_length:
+        perplexities += [None] * (max_length - len(perplexities))
 
 
 df = pd.DataFrame(all_perplexities)
@@ -34,9 +42,9 @@ df.insert(0, "Step", steps)
 df.to_csv('perplexity_results.csv', index=False)
 
 # create a line chart based on the df
-plt.plot(df['Step'], df['baseline.log'], label='baseline', color='#FF00FF')
-plt.plot(df['Step'], df['pre.log'], label='pre', color = "#009999") 
-# plt.plot(df['Step'], df['post.log'], label='post', color = "#FFFF00")
+plt.plot(df['Step'], df['baseline.log'], label='baseline', color='#FF00FF') 
+plt.plot(df['Step'], df['post.log'], label='post', color = "#FFFF00")
+plt.plot(df['Step'], df['pre.log'], label='pre', color = "#009999")
 plt.xlabel('Step')
 plt.ylabel('Ppl')
 plt.legend()
